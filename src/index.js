7d96102ec34274b9f9f1a8ffb1dcd6574289d3d6
didichulam <140446190+didichulam@users.noreply.github.com>
2024-05-04 16:39:43 +0100

    added javascript to the form and included typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function generateAffirmations(event) {
+  event.preventDefault();
 
+  new Typewriter("#affirmations-space", {
+    strings: "I get better every single day!",
+    autoStart: true,
+    cursor: null,
+  });
+}
+
+let affirmationsGenerator = document.querySelector(
+  "#affirmations-generator-form"
+);
+affirmationsGenerator.addEventListener("submit", generateAffirmations);
