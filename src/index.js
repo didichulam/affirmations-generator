1fa6d7b9c04ef77b4c9a380cb0c4685e697d5036
didichulam <140446190+didichulam@users.noreply.github.com>
2024-05-06 15:29:55 +0100

    added the AI api integration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-function generateAffirmations(event) {
-  event.preventDefault();
-
+function displayAffirmations(response) {
   new Typewriter("#affirmations-space", {
-    strings: "I get better every single day!",
+    strings: response.data.answer,
     autoStart: true,
     cursor: null,
   });
+}
+
+function generateAffirmations(event) {
+  event.preventDefault();
+  let instructionInput = document.querySelector("#instruction-space");
+  let apiKey = "fc07ae9o4bd8db7562f510t9323275bb";
+  let prompt = `please generate positive affirmations for someone who wants to speak to themselves about ${instructionInput}`;
+  let context =
+    "you are a supportive AI assistant who takes mental health very seriously and gives simple responses. Your mission is to generate four lines of positive words of affirmation using HTML format e.g <p> affirmation </p>";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayAffirmations);
 }
 
 let affirmationsGenerator = document.querySelector(
