fcb843dd03673730061b194d6ac1722f24aa6164
didichulam <140446190+didichulam@users.noreply.github.com>
2024-08-26 19:39:07 +0100

    minor changes for better performance and look

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generateAffirmations(event) {
   let apiKey = "fc07ae9o4bd8db7562f510t9323275bb";
   let prompt = `please generate affirmations about ${instructionInput.value}`;
   let context =
-    "you are a supportive AI assistant with vast knowledge about affirmations. Your mission is to generate four lines of affirmations using HTML format e.g <p> affirmation </p>. Each affirmation should be on a new line. Please keep your responses short and simple";
+    "you are a supportive AI assistant with vast knowledge about affirmations. Your mission is to generate four lines of affirmations using the following format <p> affirmation </p>. Each affirmation should be on a new line. Please keep your responses short and simple";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let affirmationsElement = document.querySelector("#affirmations-space");
